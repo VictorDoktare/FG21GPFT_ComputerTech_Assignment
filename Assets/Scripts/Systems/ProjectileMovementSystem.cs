f96ca9597326b930f2b905ecf5d11670160f81cd
Victor Doktare <victor@doktare.com>
2022-12-15 19:12:08 +0100

    Refactor Projectile Movement  Done

--- Assets/Scripts/Systems/ProjectileMovementSystem.cs
+++ Assets/Scripts/Systems/ProjectileMovementSystem.cs
@@ -1,26 +1,38 @@
 using Components;
 using Components.Tags;
 using Unity.Entities;
+using Unity.Mathematics;
 using Unity.Transforms;
+using UnityEngine;
 
 
 namespace Systems
 {
-    //public partial class ProjectileMovementSystem : SystemBase
-    //{
-    //    protected override void OnUpdate()
-    //    {
-    //        var operation = new Operation(Time.DeltaTime);
-//
-    //        Entities
-    //            .WithAll<ProjectileTag>()
-    //            .ForEach((ref Translation translation, ref Velocity velocity, in LocalToWorld localToWorld) =>
-    //            {
-    //                //Move projectile in forward facing direction
-    //                velocity.Direction = localToWorld.Up.xy;
-    //                translation.Value.xy += operation.SetVelocity(ref velocity);
-    //                
-    //            }).ScheduleParallel();
-    //    }
-    //}
+    public partial class ProjectileMovementSystem : SystemBase
+    {
+        protected override void OnUpdate()
+        {
+            var deltaTime = Time.DeltaTime;
+            
+            Entities
+                .WithAll<ProjectileTag>()
+                .ForEach((
+                    ref Translation translation,
+                    ref Velocity velocity,
+                    in LocalToWorld localToWorld) =>
+                {
+                    //Move projectile in forward facing direction
+                    velocity.Direction = new Vector3(
+                        localToWorld.Up.x,
+                        localToWorld.Up.y,
+                        localToWorld.Up.z).normalized;
+                    
+                    translation.Value.xyz += new float3(
+                        velocity.Direction.x,
+                        velocity.Direction.y,
+                        velocity.Direction.z) * velocity.Speed * deltaTime;
+                    
+                }).ScheduleParallel();
+        }
+    }
 }
